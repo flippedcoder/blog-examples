c2e4547064ffc888d890f9903e9fea914558882c
flippedcoder <milecia.mcgregor@aol.com>
2021-05-10 07:37:23 -0500

    update variable names

--- 3d-image-transformation/src/App.jsx
+++ 3d-image-transformation/src/App.jsx
@@ -2,71 +2,75 @@ import React from "react";
 import { Image, Transformation, CloudinaryContext } from "cloudinary-react";
 
 function App() {
-  const mainImgVariables = {
+  const baseDims = {
     w: 500,
-    h: 500,
+    h: 300,
     dp: 15,
   };
 
-  const secImgVariables = {
-    hadp: mainImgVariables.h + mainImgVariables.dp,
-    wadp: mainImgVariables.w + mainImgVariables.dp,
+  const secDims = {
+    hadp: baseDims.h + baseDims.dp,
+    wadp: baseDims.w + baseDims.dp,
   };
+
+  const imagePaths = ["beach-boat", "fish-vegetables"];
 
   return (
     <CloudinaryContext cloudName="milecia">
-      <div>
-        <Image publicId="beach-boat.jpg">
-          <Transformation
-            width={mainImgVariables.w}
-            height={mainImgVariables.h}
-            crop="fill"
-          />
-          <Transformation
-            width={mainImgVariables.w}
-            height={mainImgVariables.h}
-            overlay="beach-boat.jpg"
-            opacity="60"
-            border="1px_solid_rgb:FFFFFF"
-            crop="fill"
-          />
-          <Transformation
-            width={mainImgVariables.dp}
-            height={mainImgVariables.h}
-            gravity="east"
-            crop="crop"
-          />
-          <Transformation angle="hflip" />
-          <Transformation
-            effect={`distort:0:0:${mainImgVariables.dp}:${mainImgVariables.dp}:${mainImgVariables.dp}:${secImgVariables.hadp}:0:${mainImgVariables.h}`}
-          />
-          <Transformation
-            x={`${mainImgVariables.dp} * -1`}
-            gravity="north_east"
-            flags="layer_apply"
-          />
-          <Transformation
-            width={mainImgVariables.w}
-            height={mainImgVariables.h}
-            overlay="beach-boat.jpg"
-            opacity="60"
-            border="1px_solid_rgb:FFFFFF"
-            crop="fill"
-          />
-          <Transformation
-            width={mainImgVariables.w}
-            height={mainImgVariables.dp}
-            gravity="south"
-            crop="crop"
-          />
-          <Transformation angle="vflip" />
-          <Transformation
-            effect={`distort:0:0:${mainImgVariables.w}:0:${secImgVariables.wadp}:${mainImgVariables.dp}:${mainImgVariables.dp}:${mainImgVariables.dp}`}
-          />
-          <Transformation gravity="south" flags="layer_apply" />
-          <Transformation quality="auto" fetchFormat="auto" />
-        </Image>
-      </div>
+      {imagePaths.map((imgPath) => (
+        <div>
+          <Image publicId={imgPath}>
+            <Transformation
+              width={baseDims.w}
+              height={baseDims.h}
+              crop="fill"
+            />
+            <Transformation
+              width={baseDims.w}
+              height={baseDims.h}
+              overlay={imgPath}
+              opacity="60"
+              border="1px_solid_rgb:FFFFFF"
+              crop="fill"
+            />
+            <Transformation
+              width={baseDims.dp}
+              height={baseDims.h}
+              gravity="east"
+              crop="crop"
+            />
+            <Transformation angle="hflip" />
+            <Transformation
+              effect={`distort:0:0:${baseDims.dp}:${baseDims.dp}:${baseDims.dp}:${secDims.hadp}:0:${baseDims.h}`}
+            />
+            <Transformation
+              x={`${baseDims.dp} * -1`}
+              gravity="north_east"
+              flags="layer_apply"
+            />
+            <Transformation
+              width={baseDims.w}
+              height={baseDims.h}
+              overlay={imgPath}
+              opacity="60"
+              border="1px_solid_rgb:FFFFFF"
+              crop="fill"
+            />
+            <Transformation
+              width={baseDims.w}
+              height={baseDims.dp}
+              gravity="south"
+              crop="crop"
+            />
+            <Transformation angle="vflip" />
+            <Transformation
+              effect={`distort:0:0:${baseDims.w}:0:${secDims.wadp}:${baseDims.dp}:${baseDims.dp}:${baseDims.dp}`}
+            />
+            <Transformation gravity="south" flags="layer_apply" />
+            <Transformation quality="auto" fetchFormat="auto" />
+          </Image>
+        </div>
+      ))}
     </CloudinaryContext>
   );
 }
