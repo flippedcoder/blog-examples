78c9de4bc58cdc916355e0bc862183f0398bcdd7
flippedcoder <milecia.mcgregor@aol.com>
2021-05-23 15:58:35 -0500

    added vue harperdb project

---
+++ vue-harperdb/src/components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header>
+    <img alt="Vue logo" src="../assets/logo.png" height="50">
+    <p>My Vue.js Application</p>
+  </header>
+</template>
---
+++ vue-harperdb/src/components/Users.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h3>Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id">
+          <th scope="row">{{ user.id }}</th>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.address.city }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: null,
+    };
+  },
+  // replace this with HarperDB
+  created: function() {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      this.users = res.data;
+    });
+  },
+};
+</script>
+
+<style>
+h3 {
+  margin-bottom: 5%;
+}
+</style>
