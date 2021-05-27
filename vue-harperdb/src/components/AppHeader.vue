485ca40e9c7816d4683448f5b2ccadb5b89a5a96
flippedcoder <milecia.mcgregor@aol.com>
2021-05-26 22:39:01 -0500

    updated vue harperdb app

--- vue-harperdb/src/components/AppHeader.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <header>
-    <img alt="Vue logo" src="../assets/logo.png" height="50">
-    <p>My Vue.js Application</p>
-  </header>
-</template>
--- vue-harperdb/src/components/Users.vue
+++ vue-harperdb/src/components/Users.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h3>Users:</h3>
+    <div class="flex">
+      <h3>Users:</h3>
+      <button v-on:click="showForm = true">Add new user</button>
+    </div>
     <table class="table">
       <thead>
         <tr>
@@ -15,27 +18,106 @@
           <th scope="row">{{ user.id }}</th>
           <td>{{ user.name }}</td>
           <td>{{ user.email }}</td>
-          <td>{{ user.address.city }}</td>
+          <td>{{ user.city }}</td>
         </tr>
       </tbody>
     </table>
+    <div v-if="showForm">
+      <form>
+        <div>
+          <label>Name</label>
+          <input type="text" v-model="formData.name" />
+        </div>
+        <div>
+          <label>City</label>
+          <input type="text" v-model="formData.city" />
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="text" v-model="formData.email" />
+        </div>
+        <button type="submit" v-on:click="addUser(formData)">Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   name: "Users",
   data() {
     return {
       users: null,
+      errored: null,
+      loading: null,
+      showForm: false,
+      formData: {
+        name: "",
+        email: "",
+        city: "",
+      },
     };
   },
-  // replace this with HarperDB
-  created: function() {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      this.users = res.data;
-    });
+  mounted() {
+    axios
+      .post(
+        "https://cloud-0-milecia.harperdbcloud.com",
+        JSON.stringify({
+          operation: "sql",
+          sql: "select * from staging.users",
+        }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: process.env.VUE_APP_HARPERDB_TOKEN,
+          },
+        }
+      )
+      .then((response) => {
+        this.users = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+  methods: {
+    addUser: function(data) {
+      console.log(data)
+      axios
+        .post(
+          "https://cloud-0-milecia.harperdbcloud.com",
+          JSON.stringify({
+            operation: "insert",
+            schema: "staging",
+            table: "users",
+            records: [
+              {
+                name: data.name,
+                city: data.city,
+                email: data.email,
+              },
+            ],
+          }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: process.env.VUE_APP_HARPERDB_TOKEN,
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    },
   },
 };
 </script>
@@ -43,5 +125,9 @@ export default {
 <style>
 h3 {
   margin-bottom: 5%;
+}
+
+.flex {
+  display: flex;
 }
 </style>
