4e2e9a0c356787f1d36615a645fb4db539043117
flippedcoder <milecia.mcgregor@aol.com>
2021-06-14 17:21:15 -0500

    added dashboard layout

--- redwood-user-dashboard/web/src/Routes.js
+++ redwood-user-dashboard/web/src/Routes.js
@@ -12,24 +12,27 @@ import { Set, Router, Route } from '@redwoodjs/router'
 import TeamsLayout from 'src/layouts/TeamsLayout'
 
 import UsersLayout from 'src/layouts/UsersLayout'
+import DashboardLayout from './layouts/DashboardLayout/DashboardLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={TeamsLayout}>
-        <Route path="/teams/new" page={NewTeamPage} name="newTeam" />
-        <Route path="/teams/{id:Int}/edit" page={EditTeamPage} name="editTeam" />
-        <Route path="/teams/{id:Int}" page={TeamPage} name="team" />
-        <Route path="/teams" page={TeamsPage} name="teams" />
-      </Set>
-      <Set wrap={UsersLayout}>
-        <Route path="/users/new" page={NewUserPage} name="newUser" />
-        <Route path="/users/{id:Int}/edit" page={EditUserPage} name="editUser" />
-        <Route path="/users/{id:Int}" page={UserPage} name="user" />
-        <Route path="/users" page={UsersPage} name="users" />
+      <Set wrap={DashboardLayout}>
+        <Set wrap={TeamsLayout}>
+          <Route path="/teams/new" page={NewTeamPage} name="newTeam" />
+          <Route path="/teams/{id:Int}/edit" page={EditTeamPage} name="editTeam" />
+          <Route path="/teams/{id:Int}" page={TeamPage} name="team" />
+          <Route path="/teams" page={TeamsPage} name="teams" />
+        </Set>
+        <Set wrap={UsersLayout}>
+          <Route path="/users/new" page={NewUserPage} name="newUser" />
+          <Route path="/users/{id:Int}/edit" page={EditUserPage} name="editUser" />
+          <Route path="/users/{id:Int}" page={UserPage} name="user" />
+          <Route path="/users" page={UsersPage} name="users" />
+        </Set>
+        <Route path="/dashboard" page={DashboardPage} name="dashboard" />
       </Set>
       <Route path="/" page={LoginPage} name="login" />
-      <Route path="/dashboard" page={DashboardPage} name="dashboard" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
--- redwood-user-dashboard/web/src/layouts/DashboardLayout/DashboardLayout.js
+++ redwood-user-dashboard/web/src/layouts/DashboardLayout/DashboardLayout.js
@@ -1,5 +1,26 @@
+import styled from 'styled-components'
+
 const DashboardLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <FlexBox>
+      <SideNav></SideNav>
+      <FullPage>{children}</FullPage>
+    </FlexBox>
+  )
 }
+
+const FlexBox = styled.div`
+  display: flex;
+`
+
+const FullPage = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+
+const SideNav = styled.nav`
+  border-right: 1px solid;
+  width: 250px;
+`
 
 export default DashboardLayout
